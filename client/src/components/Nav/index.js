170bdf76b1f3197aa05de90921160e1c2b88d817
Justin Puzey <justpuzey@gmail.com>
2021-07-23 12:13:22 -0400

    add scaffold files and directories for client

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav() {
+
+  return (
+    <header>
+      <h1>
+        GitShot
+      </h1>
+    </header>
+  );
+}
+
+export default Nav;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+// Import components here
+import ProductList from "../components/Nav";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <p>This is placeholder text</p>
+    </div>
+  );
+};
+
+export default Home;
